db6f66b472bbc7c7b5a46ada7191dd7ccf267d2c
Matthew The <matthewthe@gmail.com>
2018-02-15 14:03:37 +0100

    Added pi0 correction

--- bin/generate_mimic_peptide_report.py
+++ bin/generate_mimic_peptide_report.py
@@ -9,18 +9,70 @@ plotIdx_ = 0
 # FOMR = false omission rate
 # FDR = false discovery rate
 
-def main():
+def main(argv):
   #percPeptFile = "/media/storage/mergespec/data/103111-Yeast-2hr/percolator_tandem_tdc/tab/103111-Yeast-2hr.percolator.tab.peptides"
   #percPeptFile = "/media/storage/mergespec/data/Percolator_system_tests/percolator_tdc/tab/Percolator_system_tests.percolator.tab.peptides"
   #percPeptFile = "/media/storage/mergespec/data/Percolator_system_tests/percolator_tdc_concat/tab/Percolator_system_tests.percolator.tab.peptides"
-  percPeptFile = sys.argv[1]
   fastaFile = "/home/matthew/data/db/swissprot_yeast_160315.with_mimic_s0.04.fasta"
   samplePeptides = getSamplePeptides(open(fastaFile, 'rb'))
-  print percPeptFile
   print "NumSamplePeptides = ", len(samplePeptides)
-  qvalsReported, qvalsObserved = getQvalues(percPeptFile, samplePeptides)
-  plotQvalues(qvalsReported, qvalsObserved)
+  
+  maxNumIds = 0
+  for percPeptFile in argv:
+    print percPeptFile
+    qvalsReported, qvalsObserved, numIds = getQvaluesAndNumIds(percPeptFile, samplePeptides)
+    label = percPeptFile.split('/')[-2].replace('tab_','').replace('_', '\_')
+    plotQvalues(qvalsReported, qvalsObserved, label = label)
+    plotPerformance(qvalsReported, qvalsObserved, numIds, label = label)
+    m = (max(numIds[:bisect.bisect_right(qvalsReported, 0.05)]) / 1000 + 1) * 1000
+    maxNumIds = max([m, maxNumIds])
+  
+  xlabel = "Reported q value"
+  ylabel = "Observed FDR"
+  
+  plt.figure(1)
+  plt.subplot(1, 2, 1)
+  x = np.linspace(0, 1, num=1000)
+  plt.plot(x, x, 'k--')
+  plt.plot(x, x / 1.116, 'k:') # on PSM level we expect 1 extra false positive from the sample database, for each 9 false positives to the entrapment database
+  plt.axis([0, 1, 0, 1])
+  plt.xlabel(xlabel, fontsize = 18)
+  plt.ylabel(ylabel, fontsize = 18)
+  plt.legend(loc = 'upper left')
+  
+  plt.subplot(1, 2, 2)
+  plt.plot(x, x, 'k--')
+  plt.plot(x, x / 1.116, 'k:') # on PSM level we expect 1 extra false positive from the sample database, for each 9 false positives to the entrapment database
+  plt.axis([0, 0.1, 0, 0.1])
+  plt.xlabel(xlabel, fontsize = 18)
+  plt.ylabel(ylabel, fontsize = 18)
+  plt.legend(loc = 'upper left')
+  
   plt.tight_layout()
+  
+  xlabel = "Reported q value"
+  ylabel = "Num significant PSMs"
+
+  plt.figure(2)
+  plt.subplot(1, 2, 1)
+  x = np.linspace(0, 1, num=1000)
+  plt.plot([0.01, 0.01], [0, maxNumIds], 'k--')
+  plt.axis([0, 0.05, 0, maxNumIds])
+  plt.xlabel(xlabel, fontsize = 18)
+  plt.ylabel(ylabel, fontsize = 18)
+  plt.legend(loc = 'lower right')
+  
+  xlabel = "Observed q value"
+  
+  plt.subplot(1, 2, 2)
+  plt.plot([0.01, 0.01], [0, maxNumIds], 'k--')
+  plt.axis([0, 0.05, 0, maxNumIds])
+  plt.xlabel(xlabel, fontsize = 18)
+  plt.ylabel(ylabel, fontsize = 18)
+  plt.legend(loc = 'lower right')
+  
+  plt.tight_layout()
+  
   plt.show()
 
 def getPeptideByTrypticDigest(seq):
@@ -57,7 +109,7 @@ def cleanPeptide(peptide):
     i += 1  
   return cleanPeptide
   
-def getQvalues(percPeptFile, samplePeptides):
+def getQvaluesAndNumIds(percPeptFile, samplePeptides):
   file = open(percPeptFile, 'rb') # The input is the peptide output file (.tab) from Percolator (-l)
   reader = csv.reader(file, delimiter='\t')
   reader.next()
@@ -84,7 +136,7 @@ def getQvalues(percPeptFile, samplePeptides):
   print "Number of identifications at reported q-val < 0.01:", numIds[bisect.bisect_left(qvalsReported, 0.01)]
   print "Number of identifications at observed q-val < 0.01:", numIds[bisect.bisect_left(qvalsObserved, 0.01)]
   
-  return qvalsReported, qvalsObserved
+  return qvalsReported, qvalsObserved, numIds
 
 def fdrsToQvals(fdrs):
   qvals = [0] * len(fdrs)
@@ -93,33 +145,22 @@ def fdrsToQvals(fdrs):
     qvals[i] = min(qvals[i+1], fdrs[i])
   return qvals
     
-def plotQvalues(qvalsReported, qvalsObserved):
-  global plotIdx_
+def plotQvalues(qvalsReported, qvalsObserved, label = ""):  
+  plt.figure(1)
+  plt.subplot(1, 2, 1)
+  plt.plot(qvalsReported, qvalsObserved, label = label)
   
-  xlabel = "Reported q value"
-  ylabel = "Observed FDR"
+  plt.subplot(1, 2, 2)
+  plt.plot(qvalsReported, qvalsObserved, label = label)
 
-  plotIdx_ += 1
+def plotPerformance(qvalsReported, qvalsObserved, numIds, label = ""):
+  plt.figure(2)
+  plt.subplot(1, 2, 1)
+  plt.plot(qvalsReported, numIds, label = label)
   
-  plt.subplot(1, 2, plotIdx_)
-  x = np.linspace(0, 1, num=1000)
-  plt.plot(qvalsReported, qvalsObserved)
-  plt.plot(x, x, 'k--')
-  plt.plot(x, x * 0.9, 'k:') # on PSM level we expect 1 extra false positive from the sample database, for each 9 false positives to the entrapment database
-  plt.axis([0, 1, 0, 1])
-  plt.xlabel(xlabel, fontsize = 18)
-  plt.ylabel(ylabel, fontsize = 18)
+  plt.subplot(1, 2, 2)
+  plt.plot(qvalsObserved, numIds, label = label)
   
-  plotIdx_ += 1
-  
-  plt.subplot(1, 2, plotIdx_)
-  plt.plot(qvalsReported, qvalsObserved)
-  plt.plot(x, x, 'k--')
-  plt.plot(x, x * 0.9, 'k:') # on PSM level we expect 1 extra false positive from the sample database, for each 9 false positives to the entrapment database
-  plt.axis([0, 0.1, 0, 0.1])
-  plt.xlabel(xlabel, fontsize = 18)
-  plt.ylabel(ylabel, fontsize = 18)
-   
 if __name__ == "__main__":
-  main()
+  main(sys.argv[1:])
 
--- bin/generate_prest_peptide_report.py
+++ bin/generate_prest_peptide_report.py
@@ -8,12 +8,16 @@ plotIdx_ = 0
 # FOMR = false omission rate
 # FDR = false discovery rate
 
-def main():
+def main(argv):
   #percTargetFile = "/media/storage/mergespec/data/FE_Kall_vial_v2_%d/percolator_tdc/tab/FE_Kall_vial_v2_%d.percolator.tab.peptides"
   #vials = range(1,4)
   
-  percTargetFile = "/media/storage/mergespec/data/FE_Kall_vial_%s/percolator_tdc/tab/FE_Kall_vial_%s.percolator.tab.peptides"
-  vials = ["A","B","AB"]
+  #percTargetFile = "/media/storage/mergespec/data/FE_Kall_vial_%s/percolator_tdc/tab/FE_Kall_vial_%s.percolator.tab.peptides"
+  #percTargetFile = "/media/storage/mergespec/data/FE_Kall_vial_%s/percolator_tide_concat/tab_tdc/FE_Kall_vial_%s.percolator.tab.peptides"
+  #vials = ["A","B","AB"]
+  
+  percTargetFile = argv[0]
+  vials = ["AB"]
   
   poolAFastaFile = "/media/storage/mergespec/data/db/prest_pool_a.fasta"
   poolBFastaFile = "/media/storage/mergespec/data/db/prest_pool_b.fasta"
@@ -22,13 +26,14 @@ def main():
   poolBProteins = list(getProteinIds(open(poolBFastaFile,'r')))
   
   for i in vials:
-    percPeptFile = percTargetFile % (i,i)
+    #percPeptFile = percTargetFile % (i,i)
+    percPeptFile = percTargetFile
     print percPeptFile
-    if i == vials[0]:
+    if i == "A":
       presentProteins = poolAProteins
-    elif i == vials[1]:
+    elif i == "B":
       presentProteins = poolBProteins
-    elif i == vials[2]:
+    elif i == "AB":
       presentProteins = poolAProteins + poolBProteins
     qvals, fdrs = getQvalues(percPeptFile, presentProteins)
     plotQvalues(qvals, fdrs)
@@ -54,6 +59,8 @@ def getQvalues(percPeptFile, presentProteins):
   qvals = []
   fdrs = []
   
+  dbCorrection = 1.0 - (len(presentProteins) / 1383.0) # for every (1383 - presentProteins) false positives, we expect 1383 false positives
+  
   print "\t".join(["PSMId", "PercolatorScore", "reportedQvalue", "observedFDR", "peptide", "protein"])
   for row in reader:
     reportedQvalue = float(row[2])
@@ -74,7 +81,7 @@ def getQvalues(percPeptFile, presentProteins):
       fp = fp + 1 # Actual false positives
       if fp <= 20:
         print "\t".join(row[:2] + [str(reportedQvalue), str(round(float(fp) / (tp + fp),4))] + row[4:])
-    fdr = float(fp) / (tp + fp)
+    fdr = float(fp) / dbCorrection / (tp + fp)
     
     qvals.append(reportedQvalue)
     fdrs.append(fdr)
@@ -106,5 +113,5 @@ def plotQvalues(qvals, fdrs):
   plt.ylabel(ylabel, fontsize = 18)
    
 if __name__ == "__main__":
-  main()
+  main(sys.argv[1:])
 
